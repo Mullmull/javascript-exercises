52dd10eee0a3f2359635b9a1cc4d8396c9c8c042
Mullmull <leben@diakonie-hamburg.de>
2023-11-21 15:50:41 +0100

    first 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,nummberOfTimes) {
+    let newStr=""
+    if (nummberOfTimes < 0) {
+        return "ERROR"
+    }
+    for (let index = 0; index < nummberOfTimes ; index++) {
+        newStr +=str
+    }
+        return newStr
 };
 
 // Do not edit below this line
